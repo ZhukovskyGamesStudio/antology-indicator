923204f18441d01f81398914fe86bbad077aff08
GrafEnters <graf.enters@gmail.com>
2026-04-06 20:42:57 +0400

    escape fix

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -28,25 +28,29 @@ public class UI : MonoBehaviour {
         }
 
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            EscapePanel.gameObject.SetActive(!EscapePanel.gameObject.activeSelf);
-
-            if (EscapePanel.activeSelf) {
-                canMove = FirstPersonController.playerCanMove;
-                canRotate = FirstPersonController.cameraCanMove;
-                FirstPersonController.playerCanMove = false;
-                FirstPersonController.cameraCanMove = false;
-            } else {
-                FirstPersonController.playerCanMove = canMove;
-                FirstPersonController.cameraCanMove = canRotate;
-            }
-            
-            
-            Time.timeScale = EscapePanel.gameObject.activeSelf ? 0 : 1;
-            //AudioListener.volume = EscapePanel.gameObject.activeSelf ? 0.2f : 1;
-            OtherUi.SetActive(!EscapePanel.gameObject.activeSelf);
-            Cursor.visible = EscapePanel.gameObject.activeSelf;
-            Cursor.lockState = EscapePanel.gameObject.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
+            OnPressedEsc();
         }
+    }
+
+    public void OnPressedEsc() {
+        EscapePanel.gameObject.SetActive(!EscapePanel.gameObject.activeSelf);
+
+        if (EscapePanel.activeSelf) {
+            canMove = FirstPersonController.playerCanMove;
+            canRotate = FirstPersonController.cameraCanMove;
+            FirstPersonController.playerCanMove = false;
+            FirstPersonController.cameraCanMove = false;
+        } else {
+            FirstPersonController.playerCanMove = canMove;
+            FirstPersonController.cameraCanMove = canRotate;
+        }
+            
+            
+        Time.timeScale = EscapePanel.gameObject.activeSelf ? 0 : 1;
+        //AudioListener.volume = EscapePanel.gameObject.activeSelf ? 0.2f : 1;
+        OtherUi.SetActive(!EscapePanel.gameObject.activeSelf);
+        Cursor.visible = EscapePanel.gameObject.activeSelf;
+        Cursor.lockState = EscapePanel.gameObject.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
     }
 
     public void Restart() {
